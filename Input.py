b853991ef54708aec490c60e3b0bcb1942ed96e6
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2025-08-22 01:17:08 +0500

    Notes 4

---
+++ Input.py
@@ -0,0 +1,9 @@
+a=25
+b=5
+print(a+b)
+input1=input("Enter your number: ")
+input2=input("Enter your second number: ")
+print(input1+input2)
+input1=int(input("Enter your number: "))
+input2=int(input("Enter your second number: "))
+print(input1+input2)
