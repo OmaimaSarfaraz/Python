af52309c499467f3305d759395194f1ac3937947
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2025-08-26 13:29:35 +0500

    Notes 8

---
+++ Strings.py
@@ -0,0 +1,16 @@
+a = 'Hello'
+b = "World"
+c = """This is
+a multiline
+string"""
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[4])
+print(b[:])
+print(b[0:2])
+print(b[0:4])
+print(b[1:3])
+print(b[-2:])
+print(a+" "+b)
+print(a*3)
