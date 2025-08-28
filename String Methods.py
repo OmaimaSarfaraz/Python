6bfe686a342b355e51aa54f61b4eb6f4b1f3a84a
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2025-08-28 13:32:16 +0500

    Notes 10

---
+++ String Methods.py
@@ -0,0 +1,10 @@
+text = "   Python Programming   "
+
+print(text.lower())       # python programming
+print(text.upper())       # PYTHON PROGRAMMING
+print(text.strip())       # Python Programming
+print(text.replace("Python","Java"))  # Java Programming
+print(text.find("Pro"))   # 10
+print(text.count("m"))    # 2
+print(text.startswith("Py"))  # False
+print(text.endswith("ing"))   # False
