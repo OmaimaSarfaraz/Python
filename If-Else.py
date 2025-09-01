fe2e1d8287003f771ce25fb78d65425a83e9df9a
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2025-09-01 14:46:55 +0500

    Notes 14

---
+++ If-Else.py
@@ -0,0 +1,16 @@
+#If
+age = 20  
+
+# If condition checks whether age is greater than or equal to 18
+if age >= 18:  
+    print("You can vote")   # This will run because age = 20, which is >= 18
+
+
+#If-Else
+age = 16  
+
+# If condition checks age
+if age >= 18:  
+    print("You can vote")   # Runs only if age >= 18
+else:  
+    print("You cannot vote")  # Runs when condition is False
