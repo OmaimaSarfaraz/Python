abd88a06632a46ce213b24fd737dad3030bd9a14
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2025-09-04 16:22:32 +0500

    Notes 17

---
+++ For Loop.py
@@ -0,0 +1,16 @@
+# Print numbers from 1 to 5
+for i in range(1, 6):   # starts from 1, ends before 6
+    print(i)
+
+# Print Table of 2
+for i in range(1, 11):   # 1 to 10
+    print("2 x", i, "=", 2*i)
+
+# Loop through a String
+for char in "Python":
+    print(char)
+
+# Loop through a List
+fruits = ["Apple", "Banana", "Mango"]
+for fruit in fruits:
+    print("I like", fruit)
