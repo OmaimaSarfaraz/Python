fb4795b766cd8eef1249e1c710cbeed2a1c4f769
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2025-09-05 18:11:35 +0500

    Notes 18

---
+++ Range Function.py
@@ -0,0 +1,31 @@
+# range(end)
+for i in range(5):  
+    print(i)
+print("\n")
+
+# range(start, end)
+for i in range(1, 6):
+    print(i)
+print("\n")
+
+# range(start, end, step)
+for i in range(2, 11, 2):
+    print(i)
+print("\n")
+
+# Odd numbers (1 to 9):
+
+for i in range(1, 10, 2):
+    print(i)
+print("\n")
+
+# Reverse Counting (10 to 1):
+
+for i in range(10, 0, -1):
+    print(i)
+print("\n")
+
+# Multiplication Table of 3:
+
+for i in range(1, 11):
+    print("3 x", i, "=", 3*i)
