edde4faebbc76e3585c219b4cf80a6ac8d0dc881
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2025-09-06 16:03:38 +0500

    Notes 19

---
+++ Loop Control.py
@@ -0,0 +1,15 @@
+# Break
+for i in range(1, 11):
+    if i == 5:
+        break
+    print(i)
+
+# Continue
+for i in range(1, 11):
+    if i == 5:
+        continue
+    print(i)
+
+# Pass
+for i in range(5):
+    pass
