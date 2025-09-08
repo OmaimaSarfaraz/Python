84743f5ad6a46d09ccf25648030259e3e124fb2e
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2025-09-08 19:40:15 +0500

    Notes 21

---
+++ Pattern in Loops.py
@@ -0,0 +1,26 @@
+#Right Angle Triangle (Stars)
+for i in range(1, 6):      
+    for j in range(1, i+1):
+        print("*", end=" ")
+    print()
+print()
+
+# Square Pattern
+for i in range(5):
+    for j in range(5):
+        print("*", end=" ")
+    print()
+print()
+
+# Number Pattern
+for i in range(1, 6):
+    for j in range(1, i+1):
+        print(j, end=" ")
+    print()
+print()
+
+# Reverse Triangle
+for i in range(5, 0, -1):
+    for j in range(1, i+1):
+        print("*", end=" ")
+    print()
