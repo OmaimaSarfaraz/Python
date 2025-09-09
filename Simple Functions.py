4f7c8bfe48ed69cec2f4ac9754bf9b9f8defe428
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2025-09-09 21:29:25 +0500

    Notes 22

---
+++ Simple Functions.py
@@ -0,0 +1,21 @@
+# Simple Function
+def greet():
+    print("Hello, welcome to Python!")
+
+greet()
+
+
+# Function with Repeated Call
+def say_hello():
+    print("Hello Students!")
+
+say_hello()
+say_hello()
+
+# Function Doing Calculation
+def add_numbers():
+    a = 5
+    b = 3
+    print("Sum is:", a + b)
+
+add_numbers()
