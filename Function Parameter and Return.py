14b558086df451b9565c5ce8d9da23cb3c5eb4b9
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2025-09-10 13:23:15 +0500

    Notes 23

---
+++ Function Parameter and Return.py
@@ -0,0 +1,22 @@
+# Function with Parameter
+def greet(name):
+    print("Hello", name, "Welcome to Python!")
+
+greet("Ali")
+greet("Sara")
+
+
+# Function with Multiple Parameters
+def add(a, b):
+    print("Sum is:", a + b)
+
+add(5, 3)
+add(10, 7)
+
+
+# Function with Return Value
+def square(x):
+    return x * x
+
+result = square(4)
+print("Square is:", result)
