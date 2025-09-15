893bb25ef8aec3c3f9145f2a386d91bd35a834e5
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2025-09-15 20:04:30 +0500

    Notes 28

---
+++ Tuple.py
@@ -0,0 +1,26 @@
+# Tuple
+my_tuple = (10, 20, 30, 40)
+print(my_tuple)     # Output: (10, 20, 30, 40)
+
+
+# Multiple Data Types
+mixed = (1, "Hello", 3.5, True)
+print(mixed)       # Output: (1, 'Hello', 3.5, True)
+
+
+# Accessing Elements
+numbers = (10, 20, 30, 40)
+print(numbers[0])          # Output: 10
+print(numbers[-1])         # Output: 40
+
+
+# Slicing Tuples
+nums = (1, 2, 3, 4, 5)  # indexing is (0, 1, 2, 3, 4) 
+print(nums[1:4])        # Output: (2, 3, 4)
+
+
+# Tuple Functions
+fruits = ("apple", "banana", "apple", "cherry")
+print(len(fruits))               # Output: 4
+print(fruits.count("apple"))     # Output: 2
+print(fruits.index("banana"))    # Output: 1
