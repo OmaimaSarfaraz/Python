056f1729d9d5e2aa8bb26ccb174ab7b946f8c4db
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2025-09-27 11:44:43 +0500

    Notes 40

---
+++ String Formatting.py
@@ -0,0 +1,18 @@
+# f-Strings
+name = "Ali"
+age = 20
+print(f"My name is {name} and I am {age} years old.")
+
+
+# format() Method
+name = "Sara"
+marks = 95
+print("Student: {}, Marks: {}".format(name, marks))
+
+print("Marks: {1}, Name: {0}".format(name, marks))
+
+
+# % Operator (Old Style)
+course = "Python"
+duration = 3
+print("Course: %s, Duration: %d months" % (course, duration))
